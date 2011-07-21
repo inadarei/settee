4a681cf6dab6831c7585f2dd720287a9d7112eea
Irakli Nadareishvili <irakli@gmail.com>
2011-07-21 18:39:34 -0400

    improved error handling and reporting

--- src/classes/restclient.class.php
+++ src/classes/restclient.class.php
@@ -14,6 +14,8 @@ class SetteeRestClient {
   private $curl;
   
   private static $curl_workers = array();
+
+  private $http_response_headers;
   
   /**
   * Singleton factory method   
@@ -35,7 +37,8 @@ class SetteeRestClient {
 
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_HEADER, 0);    
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    curl_setopt($curl, CURLOPT_HEADERFUNCTION, array($this, '_return_header_check'));
     curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($curl, CURLOPT_TIMEOUT_MS, self::HTTP_TIMEOUT);
     curl_setopt($curl, CURLOPT_FORBID_REUSE, false); // Connection-pool for CURL
@@ -43,23 +46,48 @@ class SetteeRestClient {
     $this->curl = $curl;
     
   }
+
+  /**
+  * Class destructor cleans up any resources
+  */
+  function __destruct() {
+     curl_close($this->curl);
+  }
   
   /**
   * HTTP GET
   */
   function http_get($uri, $data = array()) {
     curl_setopt($this->curl, CURLOPT_URL, $this->get_full_url($uri));    
-    curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, "GET"); 
-    return $response = curl_exec($this->curl);    
+    curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, "GET");
+    $this->http_response_headers = array();
+    $response = curl_exec($this->curl);
+    try {
+      $this->check_status();
+    } catch (SetteeRestClientException $e) {
+      throw $e;
+    }
+    return $response;
   }  
   
   /**
   * HTTP PUT
   */
   function http_put($uri, $data = array()) {
-    curl_setopt($this->curl, CURLOPT_URL, $this->get_full_url($uri));    
-    curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, "PUT"); 
-    return $response = curl_exec($this->curl);    
+    $data = (is_array($data)) ? http_build_query($data) : $data;
+    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('Content-Length: ' . strlen($data)));
+    curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
+
+    curl_setopt($this->curl, CURLOPT_URL, $this->get_full_url($uri));
+    curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, "PUT");
+    $this->http_response_headers = array();
+    $response = curl_exec($this->curl);
+    try {
+      $this->check_status();
+    } catch (SetteeRestClientException $e) {
+      throw $e;
+    }
+    return $response;
   }
 
   /**
@@ -68,9 +96,51 @@ class SetteeRestClient {
   function http_delete($uri, $data = array()) {
     curl_setopt($this->curl, CURLOPT_URL, $this->get_full_url($uri));
     curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, "DELETE");
-    return $response = curl_exec($this->curl);
+    $this->http_response_headers = array();
+    $response = curl_exec($this->curl);
+    try {
+      $this->check_status();
+    } catch (SetteeRestClientException $e) {
+      throw $e;
+    }
+    return $response;
   }
-  
+
+  /**
+   * Check http status for safe return codes
+   *
+   * @throws SetteeRestClientException
+   */
+  private function check_status() {
+    $resp_code = curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
+    if (empty($resp_code)) { return; }
+    if ($resp_code < 199 || $resp_code > 399) {
+      if (is_array($this->http_response_headers) && !empty($this->http_response_headers[0])) {
+        $header = $this->http_response_headers[0];
+        $msg = "Error connecting to CouchDB. Server returned: \"$header\". Aborting. \n";
+      } else {
+        $msg = "Error connecting to CouchDB. Server returned: \"HTTP 1.1. $resp_code\". Aborting. \n";
+      }
+
+      throw new SetteeRestClientException($msg);
+    }
+  }
+
+  /**
+   * @param  $ch
+   * @param  $header
+   * @return void
+   */
+  private function _return_header_check($ch, $header) {
+     $resp_code = curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
+     if (empty($resp_code)) { return; }
+     if ($resp_code < 199 || $resp_code > 399) {
+       $header = str_replace(array("\n", "\r"), array('', ''), $header);
+       $this->http_response_headers[] = $header;
+     }
+  }
+
+
   /**
   * Get full URL from partial one
   */
@@ -79,3 +149,5 @@ class SetteeRestClient {
     return $this->base_url . '/' . $uri;
   }
 }
+
+class SetteeRestClientException extends Exception {}
